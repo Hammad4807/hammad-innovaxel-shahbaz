09fed0fc7765d24311be7c9206ddcff3bce1ef87
Hammad4807 <hammadshahbaz4807@gmail.com>
2025-04-20 14:51:15 +0500

    Check if URL already shortened

--- app.py
+++ app.py
@@ -26,6 +26,16 @@ def create_short_url():
     if not original_url:
         return jsonify({"error": "URL is required"}), 400
 
+
+       # Check if URL already exists
+    existing = db.urls.find_one({"url": original_url})
+    if existing:
+        existing["id"] = str(existing["_id"])
+        return jsonify({
+            "message": "This URL has already been shortened.",
+            "data": existing
+        }), 200
+
     # Generate unique short code
     short_code = generate_short_code()
     while db.urls.find_one({"shortCode": short_code}):
