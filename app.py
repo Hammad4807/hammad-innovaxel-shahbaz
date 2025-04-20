5d0481cee01a79418c2117fd5f5e758cde659d6a
Hammad4807 <hammadshahbaz4807@gmail.com>
2025-04-20 14:45:07 +0500

    Added delete URL logic

--- app.py
+++ app.py
@@ -88,6 +88,15 @@ def update_url(short_code):
     updated["id"] = str(updated["_id"])
     return jsonify(updated)
 
+
+@app.route('/shorten/<short_code>', methods=['DELETE'])
+def delete_url(short_code):
+    result = db.urls.delete_one({"shortCode": short_code})
+    if result.deleted_count == 0:
+        return jsonify({"error": "Short URL not found"}), 404
+    return '', 204
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
