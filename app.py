2d128c2eca5fe8487294e7db0e3d390869b2b112
Hammad4807 <hammadshahbaz4807@gmail.com>
2025-04-20 14:37:14 +0500

    Implement logic of retrieve long URL

--- app.py
+++ app.py
@@ -53,3 +53,19 @@ def create_short_url():
     url_data["id"] = str(inserted.inserted_id)
 
     return jsonify(url_data), 201
+
+# retrive Long Url by entring short code
+@app.route('/shorten/<short_code>', methods=['GET'])
+def retrieve_url(short_code):
+    url = db.urls.find_one({"shortCode": short_code})
+    if not url:
+        return jsonify({"error": "Short URL not found"}), 404
+
+    db.urls.update_one({"shortCode": short_code}, {"$inc": {"accessCount": 1}})
+    url['id'] = str(url['_id'])
+    return jsonify(url)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
